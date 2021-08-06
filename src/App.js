cace7bdbc253908c5910e1608da7788c7ee0e5e7
Yahya Fati <yfati037@gmail.com>
2021-08-06 10:49:56 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// "@iconify/icons-akar-icons": "^1.1.17",
+// "@iconify/icons-ant-design": "^1.1.0",
+// "@iconify/icons-bx": "^1.1.2",
+// "@iconify/icons-carbon": "^1.1.5",
+// "@iconify/icons-clarity": "^1.1.1",
+// "@iconify/icons-dashicons": "^1.1.0",
+// "@iconify/icons-flat-color-icons": "^1.1.0",
+// "@iconify/icons-fluent": "^1.1.23",
+// "@iconify/react": "^1.1.4",
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App">]</div>;
 }
 
 export default App;
