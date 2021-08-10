a3d698c59a1d47c6cea89d39092ba9dd52e9770b
Yahya Fati <yfati037@gmail.com>
2021-08-10 19:02:09 +0300

    assignee button onclicks

--- src/components/main/taskinfo/AssigneeButton.js
+++ src/components/main/taskinfo/AssigneeButton.js
@@ -7,18 +7,20 @@ const AssigneeButton = ({ taskPerson }) => {
         alert("Button Clicked");
     };
 
-    // const removeClicked = (e) => {
-    //     e.preventDefault();
-    //     alert("Remove Clicked");
-    // };
+    const removeClicked = (e) => {
+        e.preventDefault();
+        alert("Remove Clicked");
+    };
 
     return (
-        <button
-            onClick={clicked}
-            className={`btn assignee_btn ${taskPerson.leader && "leader"}`}
-        >
-            <span className="assignee_name">{taskPerson.person.name}</span>
-            <AiOutlineUserDelete className="remove_icon" />
+        <button className={`btn assignee_btn ${taskPerson.leader && "leader"}`}>
+            <span onClick={clicked} className="assignee_name">
+                {taskPerson.person.name}
+            </span>
+            <AiOutlineUserDelete
+                className="remove_icon"
+                onClick={removeClicked}
+            />
         </button>
     );
 };
