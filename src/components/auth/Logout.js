a55a5bdbb545226299a20a83716d2e376b21cbbc
Yahya Fati <yfati037@gmail.com>
2021-08-28 15:16:16 +0300

    fixed bad setState

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -1,10 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../../contexts/GlobalContext";
 
 const Logout = () => {
     const { setAuth } = useContext(AuthContext);
-    setAuth({ authenticated: false, token: null });
+    useEffect(() => {
+        setAuth({ authenticated: false, token: null });
+    });
     return <Redirect to={{ pathname: "/login" }} />;
 };
 
